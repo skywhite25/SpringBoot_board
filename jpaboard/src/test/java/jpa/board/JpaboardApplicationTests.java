f9f1b27be60e3928cdf90bf38b03fe045561efb3
DICT_DEV7 <DICT_DEV7@DESKTOP-P510VID>
2021-11-15 10:16:59 +0900

    진행중

---
+++ jpaboard/src/test/java/jpa/board/JpaboardApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
